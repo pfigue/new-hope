dffb1b77aea041b61eb42fbbd33958ade502ebad
Pablo Figue <pablo.figuerola@zalando.de>
2016-04-25 12:51:05 +0200

    Adds week2 exercises: get-the-capital letters and half-truth

--- src/new_hope/week2.clj
+++ src/new_hope/week2.clj
@@ -21,3 +21,35 @@ Use like this:
  (return-fibo-nums 3) => '(1 1 2)
  (return-fibo-nums 6) => '(1 1 2 3 5 8)
  (return-fibo-nums 8) => '(1 1 2 3 5 8 13 21))
+
+
+(defn get-the-caps
+  "Return only the capital letters"
+  [ i ]
+
+  (loop [ result [] input i]
+    (if (empty? input)
+      (clojure.string/join result)
+      (let [letter (first input)]
+        (if (Character/isUpperCase letter)
+          (recur
+           (concat result [letter])
+           (rest input))
+          (recur result (rest input)))))))
+
+(facts "Only capital letters"
+       (get-the-caps "HeLlO, WoRlD!") => "HLOWRD"
+       (get-the-caps "nothing") => ""
+       (get-the-caps "$#A(*&987Zf") => "AZ")
+
+
+(defn half-truth
+  [ boolis ]
+  (not (or
+        (nil? (some true? boolis))
+        (every? true? boolis))))
+
+(facts "Some but not all of the parameters should be truth"
+       (half-truth (list true true true)) => false
+       (half-truth (list true false true)) => true
+       (half-truth (list false false false)) => false)
